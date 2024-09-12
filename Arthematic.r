ca3f33f639c5ae3116f86d409f1d4f645c5290ac
Goutam-g3 <goutamc.23.beai@acharya.ac.in>
2024-09-12 21:04:31 +0530

    Arthematic.r

---
+++ Arthematic.r
@@ -0,0 +1,26 @@
+{
+  val1=5
+  val2=5
+  paste("sum : ",val1+val2)
+  paste("diffrence",val1-val2)
+  paste("product",val1*val2)
+  paste("division",val1/val2)
+  paste("sum",val1%%val2)
+  paste("power",val1^val2)
+}  
+
+
+
+{
+  val1=as.integer(readline("Enter the first value : "))
+  val2=as.integer(readline("Enter the secound value : "))
+  paste("sum : ",val1+val2)
+  paste("diffrence",val1-val2)
+  paste("product",val1*val2)
+  paste("division",val1/val2)
+  paste("sum",val1%%val2)
+  paste("power",val1^val2)
+} 
+
+
+
