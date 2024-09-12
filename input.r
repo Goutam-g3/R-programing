c13c4203022d4e0e4a3336a01f7a9ec2fc93f812
Goutam-g3 <goutamc.23.beai@acharya.ac.in>
2024-09-12 21:03:44 +0530

    input.r

---
+++ input.r
@@ -0,0 +1,20 @@
+{
+  val1=as.integer(readline("Enter the 1st value : "))
+  val2=as.integer(readline("Entre the 2nd value : "))
+  as.integer(val3)=val1+val2
+}
+
+?readline
+?scan
+
+print("Hello")
+paste("hello","all")
+write("hello","all")
+getwd()
+setwd()
+
+{
+  name=readline(("Enter your name : "))
+  cat("Ram ram my name is",name,file="demo.txt")
+  sprintf("Namaste!! my name is %s",name)
+}
